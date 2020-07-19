d74f2f063dc5752a17e1639244540df678972b8e
leandro-wrf <leandro.ferreira.wr@gmail.com>
2020-07-19 16:04:38 -0300

    adicionei a troca de estilo do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsPlusCircleFill, BsSearch } from "react-icons/bs";
 
 import "./styles.css";
 
 const Header = () => {
-  return (
-    <header className="header-locaflix">
-      <h1>LOCAFLIX</h1>
+  const [classActive, setClassActive] = useState(true);
 
-      <div>
-        <BsSearch className="button-tmp" size={25} color="#eeeeee" />
-        <BsPlusCircleFill size={25} color="#eeeeee" />
-      </div>
+  function handleStyle() {
+    if (!classActive) {
+      return setClassActive(true);
+    }
+
+    setClassActive(false);
+  }
+
+  if (classActive) {
+    return (
+      <header className="header-locaflix">
+        <h1>LOCAFLIX</h1>
+
+        <div>
+          <button onClick={handleStyle}>
+            <BsSearch size={25} color="#eeeeee" />
+          </button>
+          <BsPlusCircleFill size={25} color="#eeeeee" />
+        </div>
+      </header>
+    );
+  }
+
+  return (
+    <header className="header-search">
+      <input placeholder="Search here..." type="text" />
+
+      <button onClick={handleStyle}>
+        <BsSearch size={25} color="#eeeeee" />
+      </button>
     </header>
   );
 };
