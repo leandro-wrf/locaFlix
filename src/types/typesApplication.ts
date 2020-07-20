8e8badb766a5608b59ae59ca148c583c8313ae49
leandro-wrf <leandro.ferreira.wr@gmail.com>
2020-07-20 17:53:40 -0300

    adicionei o tipo da const movie

---
+++ src/types/typesApplication.ts
@@ -0,0 +1,13 @@
+export interface Movie {
+  id: number;
+  urlImage: string;
+  title: string;
+  description: string;
+  director: string;
+  genres: string;
+  languages: string;
+  subtitles: string;
+  urlImdb: string;
+  release: string;
+  reviews: string;
+}
